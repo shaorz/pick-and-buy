ebc6414e006308eb2a7cfc0ace8006f3fc16ced5
Roger SHAO <shaorz@gmail.com>
2023-03-26 19:10:57 +0800

    PANB-0001 adding details

--- a_share_utils.py
+++ a_share_utils.py
@@ -1,10 +1,16 @@
-import json , requests , datetime
-import pandas as pd
-from typing import Iterable
+# -*- coding: utf-8 -*-
+
+import json
+from datetime import datetime
+from typing import Iterable , List , Mapping
+
 import easyquotation
+import pandas as pd
+import requests
+from progressbar import *
 
 
-def get_stock_pool_today ( agent: str = 'tencent', withPrefix : bool = True ) -> pd.DataFrame:
+def get_stock_pool_today ( agent: str = 'tencent' , withPrefix: bool = True ) -> Mapping:
 	"""
 	:arg
 	agent
@@ -35,9 +41,9 @@ def get_price_day_tx ( code: str , end_date: str = '' , count: int = 10 , freque
 	ms = 'qfq' + unit ;
 	stk = st [ 'data' ] [ code ]
 	buf = stk [ ms ] if ms in stk else stk [ unit ]
-	df = pd.DataFrame ( buf , columns = [ 'time' , 'open' , 'close' , 'high' , 'low' , 'volume' ] , dtype = 'float' )
-	df.time = pd.to_datetime ( df.time ) ;
-	df.set_index ( [ 'time' ] , inplace = True ) ;
+	df = pd.DataFrame ( buf , columns = [ 'time' , 'open' , 'close' , 'high' , 'low' , 'volume' ] )
+	df.time = pd.to_datetime ( df.time )
+	df.set_index ( [ 'time' ] , inplace = True )
 	df.index.name = ''
 	return df
 
@@ -53,8 +59,8 @@ def get_price_min_tx ( code: str , end_date: str = None , count: int = 10 , freq
 	df = df [ [ 'time' , 'open' , 'close' , 'high' , 'low' , 'volume' ] ]
 	df [ [ 'open' , 'close' , 'high' , 'low' , 'volume' ] ] = df [
 		[ 'open' , 'close' , 'high' , 'low' , 'volume' ] ].astype ( 'float' )
-	df.time = pd.to_datetime ( df.time ) ;
-	df.set_index ( [ 'time' ] , inplace = True ) ;
+	df.time = pd.to_datetime ( df.time )
+	df.set_index ( [ 'time' ] , inplace = True )
 	df.index.name = ''
 	df [ 'close' ] [ -1 ] = float ( st [ 'data' ] [ code ] [ 'qt' ] [ code ] [ 3 ] )
 	return df
@@ -72,14 +78,14 @@ def get_price_sina ( code: str , end_date: str = '' , count: int = 10 , frequenc
 	URL = f'http://money.finance.sina.com.cn/quotes_service/api/json_v2.php/CN_MarketData.getKLineData?symbol={code}&scale={ts}&ma=5&datalen={count}'
 	dstr = json.loads ( requests.get ( URL ).content ) ;
 	# df=pd.DataFrame(dstr,columns=['day','open','high','low','close','volume'],dtype='float')
-	df = pd.DataFrame ( dstr , columns = [ 'day' , 'open' , 'high' , 'low' , 'close' , 'volume' ] )
+	df = pd.DataFrame ( dstr , columns = [ 'time' , 'open' , 'high' , 'low' , 'close' , 'volume' ] )
 	df [ 'open' ] = df [ 'open' ].astype ( float ) ;
 	df [ 'high' ] = df [ 'high' ].astype ( float ) ;  # 转换数据类型
 	df [ 'low' ] = df [ 'low' ].astype ( float ) ;
 	df [ 'close' ] = df [ 'close' ].astype ( float ) ;
 	df [ 'volume' ] = df [ 'volume' ].astype ( float )
-	df.day = pd.to_datetime ( df.day ) ;
-	df.set_index ( [ 'day' ] , inplace = True ) ;
+	df.time = pd.to_datetime ( df.time )
+	df.set_index ( [ 'time' ] , inplace = True )
 	df.index.name = ''  # 处理索引
 	if (end_date != '') & (frequency in [ '240m' , '1200m' , '7200m' ]): return df [ df.index <= end_date ] [ -mcount: ]
 	return df
@@ -91,20 +97,107 @@ def get_price ( code: str , end_date: str = '' , count: int = 10 , frequency: st
 
 	if frequency in [ '1d' , '1w' , '1M' ]:
 		try:
-			return get_price_sina ( xcode , end_date = end_date , count = count , frequency = frequency )
-		except:
 			return get_price_day_tx ( xcode , end_date = end_date , count = count , frequency = frequency )
+		except:
+			return get_price_sina ( xcode , end_date = end_date , count = count , frequency = frequency )
 
 	if frequency in [ '1m' , '5m' , '15m' , '30m' , '60m' ]:
 		if frequency in '1m': return get_price_min_tx ( xcode , end_date = end_date , count = count , frequency = frequency )
 		try:
-			return get_price_sina ( xcode , end_date = end_date , count = count , frequency = frequency )
-		except:
 			return get_price_min_tx ( xcode , end_date = end_date , count = count , frequency = frequency )
+		except:
+			return get_price_sina ( xcode , end_date = end_date , count = count , frequency = frequency )
+
+
+def getCSVDumpFileName ( preceding_days: int = 30 ) -> str:
+	date = datetime.date.today ()
+	return str ( date ) + '-' + preceding_days + 'D-Hist-Ashares.csv'
+
+
+def get_a_share_hist_data ( preceding_days: int = 30 ) -> pd.DataFrame:
+	"""
+	Get the daily trading data for all A share stocks.
+
+	Parameters:
+	start_date (str): The start date of the data to retrieve (in the format 'YYYY-MM-DD').
+					  If not specified, defaults to the earliest available date.
+	end_date (str): The end date of the data to retrieve (in the format 'YYYY-MM-DD').
+					If not specified, defaults to the latest available date.
+
+	Returns:
+	pandas.DataFrame: A DataFrame containing the daily trading data for all A share stocks.
+	"""
+
+	# Get the list of A share symbols
+	stock_pool: Mapping [ str , Mapping ] = get_stock_pool_today ()
+	symbols: List = stock_pool.keys ()
+	pbar = ProgressBar ().start ()
+
+	# Get the daily trading data for each A share stock
+	dfs = [ ]
+	for i , symbol in enumerate ( symbols ):
+		pbar.update ( int ( i / len ( symbols ) * 100 ) )
+		df = get_price ( symbol , frequency = '1d' , count = preceding_days + 1 )
+		df [ 'code' ] = symbol
+		df [ 'date' ] = df.index
+		if df is not None:
+			dfs.append ( df )
+
+	# Combine the data for all A share stocks into a single DataFrame
+	if dfs:
+		a_share_data = pd.concat ( dfs )
+		a_share_data.reset_index ( inplace = True )
+		a_share_data = a_share_data [ [ 'date' , 'code' , 'open' , 'high' , 'low' , 'close' , 'volume' ] ]
+		a_share_data.to_csv ( getCSVDumpFileName ( preceding_days ) )
+		return a_share_data
+
+	return None
+
+
+def filter_top_stocks_by_volume_spike ( preceding_days: int = 30 , multiplier_level: int = 10 , max_percent_change: float = 0.3 ) -> List [ str ]:
+	"""
+	Filter the top stocks whose trading volume suddenly spikes compared with the preceding trading volume.
+
+	Args:
+		stock_pool (dict): A dictionary containing the A-share stock pool, with keys representing the stock ticker and values representing the stock info.
+		preceding_days (int): The number of preceding days to compare trading volumes.
+		multiplier_level (int): The multiplier level of the trading volume spike.
+		max_percent_change (float): The maximum percentage change allowed in preceding days' trading volume.
+
+	Returns:
+		List[str]: A list of the top stock tickers that meet the trading volume spike criteria.
+	"""
+
+	# Get the daily trading volume for all A share stocks
+	# a_share_data: pd.DataFrame = get_a_share_hist_data ( preceding_days = preceding_days )
+	a_share_data: pd.DataFrame = pd.read_csv ( getCSVDumpFileName ( preceding_days ) )
+	volume_data = a_share_data.pivot ( index = 'date' , columns = 'code' , values = 'volume' )
+
+	# Calculate the trading volume change over the preceding days
+	preceding_days_volume = volume_data.tail ( preceding_days + 1 ).mean ()
+	preceding_days_volume.name = 'preceding_volume'
+
+	current_day_volume = volume_data.tail ( 1 ).transpose ()
+	current_day_volume.columns = [ 'current_volume' ]
+
+	top_stocks = [ ]
+
+	# # Calculate the percentage change in the preceding days' volume
+	# percent_change_preceding_days = (volume_today - avg_volume_preceding_days) / avg_volume_preceding_days
+	#
+	# # Check if the volume spike meets the criteria
+	# if percent_change_preceding_days <= max_percent_change and volume_today >= (
+	# 		multiplier_level * avg_volume_preceding_days):
+	# 	top_stocks.append ( stock_ticker )
+	#
+	# # Sort the top stocks by their volume change percentage
+	# top_stocks.sort ( key = lambda x: (get_price ( x , 1 ).iloc [ -1 ] [ 'volume' ] / get_price ( x , preceding_days ) [
+	# 	'volume' ].mean ()) , reverse = True )
+
+	return top_stocks
 
 
 if __name__ == '__main__':
-
 	# df = get_stock_pool_today( 'timekline' )
 	# print ( 'Time K Line from QQ \n', df )
 
@@ -113,15 +206,17 @@ if __name__ == '__main__':
 
 	# df = get_stock_pool_today ( 'hkquote' )
 	# print ( 'HK Quote from QQ \n' , df )
-
-	df = get_stock_pool_today( 'tencent' )
-	print ( df ) # dict { 5444 }
+	#
+	# df = get_stock_pool_today ( 'tencent' )
+	# print ( df )  # dict { 5444 }
 	"""
 	{'name': '平安银行', 'code': 'sz000001', 'now': 12.82, 'close': 12.9, 'open': 12.85, 'volume': 62151900.0, 'bid_volume': 27364500, 'ask_volume': 34787400.0, 'bid1': 12.81, 'bid1_volume': 170500, 'bid2': 12.8, 'bid2_volume': 322100, 'bid3': 12.79, 'bid3_volume': 397500, 'bid4': 12.78, 'bid4_volume': 832200, 'bid5': 12.77, 'bid5_volume': 598300, 'ask1': 12.82, 'ask1_volume': 67600, 'ask2': 12.83, 'ask2_volume': 353200, 'ask3': 12.84, 'ask3_volume': 298200, 'ask4': 12.85, 'ask4_volume': 399400, 'ask5': 12.86, 'ask5_volume': 449500, '最近逐笔成交': '', 'datetime': datetime.datetime(2023, 3, 24, 16, 14, 3), '涨跌': -0.08, '涨跌(%)': -0.62, 'high': 12.86, 'low': 12.76, '价格/成交量(手)/成交额': '12.82/621519/796319542', '成交量(手)': 62151900, '成交额(万)': 796320000.0, 'turnover': 0.32, 'PE': 5.47, 'unknown': '', 'high_2': 12.86, 'low_2': 12.76, '振幅': 0.78, '流通市值': 2487.79, '总市值': 2487.84, 'PB': 0.68, '涨停价': 14.19, '跌停价': 11.61, '量比': 0.59, '委差': 7527.0, '均价': 12.81, '市盈(动)': 5.47, '市盈(静)': 5.47}
 	"""
+	#
+	# df = get_price ( 'sz000001' , frequency = '1d' , count = 30 )
+	# print ( '深市日线行情\n' , df )
+	#
+	# df = get_price ( '000001.XSHG' , frequency = '15m' , count = 10 )
+	# print ( '上证指数分钟线\n' , df )
 
-	df = get_price ( 'sh000001' , frequency = '1d' , count = 10 )
-	print ( '上证指数日线行情\n' , df )
-
-	df = get_price ( '000001.XSHG' , frequency = '15m' , count = 10 )
-	print ( '上证指数分钟线\n' , df )
+	stocks = filter_top_stocks_by_volume_spike ()
